aeaf392c3234ed5e14727ac70427ea53ebf1b68f
Andrew David <81461329+emperorsixpacks@users.noreply.github.com>
2022-04-26 02:39:06 +0100

    change

--- pos/settings.py
+++ pos/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
        'USER': env('USER'),
        'PASSWORD': env('PASSWORD'),
        'HOST': env('HOST'),
-       'PORT': env('PORT'),
+       'PORT': '',
    }
 }
 # Password validation
