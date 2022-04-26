6ba52aead6e4dc9ec35eb9d9ce77a6f359a4ab65
Andrew David <81461329+emperorsixpacks@users.noreply.github.com>
2022-04-26 08:29:58 +0100

    Update settings.py

--- pos/settings.py
+++ pos/settings.py
@@ -89,14 +89,10 @@ WSGI_APPLICATION = 'pos.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': env('NAME'),
-       'USER': env('USER'),
-       'PASSWORD': env('PASSWORD'),
-       'HOST': env('HOST'),
-       'PORT': '',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'crypto.sqlite3',
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
